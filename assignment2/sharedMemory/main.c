a594eb6a29117a6830bf758689dcefdd7cf82970
suzannemaquelin <slmaquelin@gmail.com>
2018-03-08 14:49:31 +0100

    not working, theory main

--- assignment2/sharedMemory/main.c
+++ assignment2/sharedMemory/main.c
@@ -10,7 +10,11 @@
 
 int pagesize;
 int cnt = 0;
-int pingpong[2];
+pid_t pid_child0, pid_child1;
+int shared_parent = 0;
+int SIGNAL;
+int[2] pingpong;
+
 
 void procPingPong(int whoami, volatile int*sharedTurnVariable) {
 	/*whoami == 0 or whoami == 1*/
@@ -20,22 +24,61 @@ void procPingPong(int whoami, volatile int*sharedTurnVariable) {
 		printf (whoami == 0 ? "Ping\n" : "...Pong\n");
 		*sharedTurnVariable = 1 - whoami;
 		write(pingpong[1], (void*)sharedTurnVariable, sizeof(int));
-		mprotect((void*)sharedTurnVariable, pagesize, PROT_READ);
+		//send signal SIGRTMIN to parent to let parent know it should read from the pipe
+		kill(getppid(), SIGRTMIN);
+		mprotect((void*)sharedTurnVariable, pagesize, PROT_NONE);
 		msg++;
 	}
 	exit(EXIT_SUCCESS);
 }
 
-void handler(int sig, siginfo_t *si, void *unused) {
-  int* value = si->si_addr;
-  int status = read(pingpong[0], value, sizeof(int));
-  printf("reading: %d %s\n", status, strerror(errno));
-  printf("Got SIGSEGV at address: 0x%lx\n", (long) si->si_addr);
-  mprotect(si->si_addr, pagesize, PROT_WRITE);
-  cnt++;
-  if (cnt == 10) {
-    exit(EXIT_FAILURE);
-  }
+void handler_child1(int sig, siginfo_t *si, void*unused) {
+	//send signal to parent to ask for access memory
+	if (sig == SIGSEGV) {
+		int status_kill = kill(getppid(), SIGNAL);
+	}
+	//if access granted, set mprotect to write and read from pipe
+	if (sig == SIGNAL) {
+		//mprotect to write
+		//we would like to update the rights of sharedTurnVariable to write instead of none, however we don't have its address
+		//read from pipe
+		//we would like to update sharedTurnVariable by reading the pipe, however we don't have its address
+	}
+}
+
+void parent_handler(int sig, siginfo_t *si, void*unused){
+	if (sig == SIGUSR1){
+		//first child wants access to memory
+		//if child has this right, write the memory to pipe and send signal to child
+		if (shared_parent == 0)
+		{
+			//write to pipe
+			write(pingpong[1], &shared_parent, sizeof(int));
+			//send signal to child			
+			int status_kill = kill(pid_child0, SIGUSR1);
+			if(status_kill < 0){
+				printf("status of kill: %d %s\n", status, strerror(errno));
+				exit(-1);
+			}
+		}
+	}
+	if (sig == SIGUSR2){
+		//second child wants access to memory
+		//if child has this right, write the memory to pipe and send signal to child
+		if (shared_parent == 1)
+		{		
+			write(pingpong[1], &shared_parent, sizeof(int));	
+			int status_kill = kill(pid_child1, SIGUSR2);
+			if (status_kill < 0){
+				printf("status of kill: %d %s\n", status, strerror(errno));
+				exit(-1);
+			}
+		}
+	}
+	if (sig == SIGRTMIN){
+		//read from pipe and update value shared_parent
+		read(pingpong[0], &shared_parent, sizeof(int));
+	}
 }
 
 
@@ -43,13 +86,14 @@ int main(int argc, char *argv[]) {
   void *buffer;
   int *p;
   struct sigaction sa;
-  
   int status;
   
   sa.sa_flags = SA_SIGINFO;
   sigemptyset(&sa.sa_mask);
-  sa.sa_sigaction = handler;
-  sigaction(SIGSEGV, &sa, NULL);
+  sa.sa_sigaction = parent_handler;
+  sigaction(SIGUSR1, &sa, NULL);
+  sigaction(SIGUSR2, &sa, NULL);
+  sigaction(SIGRTMIN, &sa, NULL);
   
   pagesize = sysconf(_SC_PAGE_SIZE);
 
@@ -60,24 +104,27 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Fatal error: aligned memory allocation failed.\n");
     exit(EXIT_FAILURE);
   }
-
-  printf("Start of region:        0x%lx\n", (long) buffer);
-  mprotect(buffer, 4*pagesize, PROT_READ);
   
   p = buffer;
-  printf("p: %d\n", *p);
+  mprotect(buffer, 4*pagesize, PROT_NONE);
 
-  //pipe(chanPing);
-  //pipe(chanPong);
   pipe(pingpong);
-  int status_write = write(pingpong[1], (void*)p, sizeof(int));
-  printf("writing: %d %s\n", status_write, strerror(errno));
-  if (fork() == 0) {
+  
+  if ((pid_child0 = fork()) == 0) {
+	SIGNAL = SIGUSR1;
+	sa.sa_sigaction = handler_child1;
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGNAL, &sa, NULL);
     procPingPong(0, p);
   }
-  if (fork() == 0) {
+  if ((pid_child1 = fork()) == 0) {
+	SIGNAL = SIGUSR2;
+	sa.sa_sigaction = handler_child2;
+	sigaction(SIGSEGV, &sa, NULL);
+	sigaction(SIGNAL, &sa, NULL);
     procPingPong(1, p);
   }
+
   waitpid(-1, &status, 0);
   waitpid(-1, &status, 0);
   return EXIT_SUCCESS;
